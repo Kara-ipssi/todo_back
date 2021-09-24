038bb7f667a77037dd620c5f7f455b69c7defca8
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2021-09-24 16:30:01 +0200

    add

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,13 +1,61 @@
-const createTask = (req, res, next)=>{
-    res.status(200).json('Task added')
+const fs = require("fs");
+
+const data = require('../database/tasks.json');
+
+const createTask = (req, res)=>{
+    const {contenu} = req.body;
+    const nextId = data.length > 0 ? data[data.length - 1].id + 1: 1;
+    if(contenu != null){
+        let newTask = {
+            "id": nextId,
+            "contenu": contenu,
+            "done": false
+        }
+        data.push(newTask)
+        fs.writeFileSync("./database/tasks.json", JSON.stringify(data), (error)=>{
+            if(error){
+                console.log(error);
+            }
+        })
+        return res.status(200).json(data)
+    }
 }
 
-const getAllTasks = (req, res, next)=>{
-    res.status(200).json('Tasks getted')
+const toggleState = (req, res) => {
+    const id = req.params.id;
+    const task = data.filter((x, key)=>{
+        if(x.id == id){
+            data[key].done = !data[key].done
+            fs.writeFileSync("./database/tasks.json", JSON.stringify(data), (error)=>{
+                if(error){
+                    console.log(error);
+                }
+            })
+            return x
+        }
+    })
+    return res.status(200).json(data)
 }
 
-const removeTask = (req, res, next)=>{
-    res.status(200).json('Task removed')
+const getAllTasks = (req, res)=>{
+    res.status(200).json(data)
 }
 
-module.exports = {createTask, getAllTasks, removeTask}
+const removeTask = async (req, res)=>{
+    const id = req.params.id;
+    data.filter((x, key)=>{
+        if(x.id == id){
+            data.splice(key, 1);
+            console.log(key)
+            fs.writeFileSync("./database/tasks.json", JSON.stringify(data), (error)=>{
+                if(error){
+                    console.log(error);
+                }
+            })
+        }
+    })
+    return res.status(200).json(data)
+}
+
+
+module.exports = {createTask, getAllTasks, removeTask, toggleState}
--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -1,9 +1,10 @@
-const { getAllTasks, createTask, removeTask } = require("../controllers/TaskController");
+const { getAllTasks, createTask, removeTask, toggleState } = require("../controllers/TaskController");
 
 const createTasksRoutes = (app) => {
     app.get("/tasks", getAllTasks)
     app.post("/tasks/new", createTask)
-    app.delete('/tasks/remove', removeTask)
+    app.put('/tasks/toggle/:id', toggleState)
+    app.delete('/tasks/remove/:id', removeTask)
 }
 
 module.exports = createTasksRoutes;
